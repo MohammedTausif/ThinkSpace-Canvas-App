a96f4b0b456fe6b39c4f78c81bf79009f5d45d42
Tausif's Macbook <iamtauseefofficial@gmail.com>
2025-02-21 23:54:51 +0530

    Added Delete icon to card ui

--- apps/canvas-frontend/components/ui/Card.tsx
+++ apps/canvas-frontend/components/ui/Card.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight, Binary, Calendar, Delete, DeleteIcon, Drumstick, Edit, Edit2, Edit3, Edit3Icon, EditIcon, FileEdit, FolderEdit, Link, LucideDelete, PaintBucket, Pencil, PencilIcon, Trash, Trash2, Users } from "lucide-react";
+import { ArrowRight, Binary, Calendar, Delete, DeleteIcon, Drumstick, Edit, Edit2, Edit3, Edit3Icon, EditIcon, FileEdit, FolderEdit, Link, Link2, Link2Off, LinkIcon, LucideDelete, PaintBucket, Pencil, PencilIcon, PencilLine, PencilOffIcon, Trash, Trash2, Users } from "lucide-react";
 
 interface CardProps{
     id: number,
@@ -17,14 +17,14 @@ export default function RoomCard({id, slug ,createdAt,  name, photo, adminId, on
                 className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden"
               >
                 <div className="p-6">
-                  <div className="flex justify-between items-start mb-4">
-                    <h2 className="text-xl font-semibold text-gray-900 flex-1">
+                  <div className="flex justify-between items-start mb-4 ">
+                    <h2 className=" text-xl font-semibold text-gray-900 flex-1">
                       {slug.toLocaleUpperCase()}
+                     
                     </h2>
-                    <button className="bg-blue-600 px-4 py-0.5 rounded-full hover:bg-white hover: hover:border-blue-600">
-                      <span className="text-sm text-white  hover:text-gray-800">
-                       Delete
-                      </span>
+
+                    <button className="" >
+                     <Trash2 className="size-4 text-gray-500 "/>
                     </button>
                   </div>
                   
@@ -53,16 +53,16 @@ export default function RoomCard({id, slug ,createdAt,  name, photo, adminId, on
 
                     <button
                     onClick={onClick}
-                     className="w-28 bg-blue-600  font-medium text-white py-2 px-4 rounded-sm hover:bg-blue-700 transition-colors duration-200 flex items-center justify-center space-x-1 overflow-hidden">
+                     className="w-[6rem] bg-blue-600  font-medium text-white py-2 px-4 rounded-sm hover:bg-blue-700 transition-colors duration-200 flex items-center justify-center space-x-1 overflow-hidden">
                       <span>Room</span>
                       <ArrowRight className="w-4 h-4 " />
                     </button>
 
                     <button
                     onClick={onClick}
-                     className="w-28 bg-white border-2 text-blue-600 border-blue-600  font-medium py-2  px-4 rounded-sm hover:text-gray-300 transition-colors duration-200 flex items-center justify-center space-x-1 overflow-hidden">
-                      <span>Delete</span>
-                      <Trash2 className="size-5 text-blue-600 hover:text-gray-400"/>
+                     className="w-[6rem] bg-white border-[1px] text-blue-600 border-blue-600  font-medium py-2  px-4 rounded-sm hover:text-white hover:bg-blue-600 transition-colors duration-200 flex items-center justify-center space-x-1 overflow-hidden">
+                      <span>Edit</span>
+                      <Pencil className="size-4 "/>
                     </button>
 
                   </div>
