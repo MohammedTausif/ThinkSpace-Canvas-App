ede90148ec643e6823c82ca31b249e8b16911fac
Tausif's Macbook <iamtauseefofficial@gmail.com>
2025-03-13 23:56:52 +0530

    Landing Ui Update

--- apps/canvas-frontend/components/Landing/Landing-page.tsx
+++ apps/canvas-frontend/components/Landing/Landing-page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import {motion} from 'framer-motion'
 import Hero from "./Hero";
 import Navbar from "../Navbar/Navbar";
+import Features from "./Features";
+import Cta from "./Cta";
+import Footer from "./Footer";
 
 const LandingPage=()=> {
   return (
@@ -20,6 +23,9 @@ const LandingPage=()=> {
             <Navbar />
             <div className="h-screen absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:23px_32px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
             <Hero/>
+            <Features/>
+            <Cta/>
+            <Footer/>
             </motion.div>
             </div>
       );
